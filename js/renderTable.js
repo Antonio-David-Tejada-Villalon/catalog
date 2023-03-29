20fbe8fea047efdd1e4eeca35201976ba57135ac
Antonio-David-Tejada-Villalon <4vdel777@gmail.com>
2023-03-29 19:45:39 -0300

    Second commit

--- js/renderTable.js
+++ js/renderTable.js
@@ -11,6 +11,7 @@ export default function renderTable(d){
 		let indexT = document.createElement('span');
 		indexT.setAttribute('id', d[i].TITULO);
 		let img = document.createElement('img');
+		img.setAttribute('class', 'box');
 		let title = document.createElement('h5');
 		let link = document.createElement('span');
 		let modal = document.createElement('div');
@@ -23,26 +24,22 @@ export default function renderTable(d){
 	
 		//modal
 		modal.innerHTML =
-		`<div class="modal fade" id="exampleModal--${i}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+		`<div class="modal fade box" id="exampleModal--${i}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
 		  <div class="modal-dialog">
 			<div class="modal-content">
-			  <div class="modal-header d-flex justify-content-between">
-			  <img src="${d[i].IMAGEN}" class="rounded float-start" alt="${d[i].TITULO}">
+			  <div class="modal-header d-flex justify-content-between flex-column">
+			  <img src="${d[i].IMAGEN}" class="rounded float-start box" alt="${d[i].TITULO}">
 				<h5 class="modal-title" id="exampleModalLabel">${d[i].TITULO}</h5>
-				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+				<h5 class="modal-title" id="exampleModalLabel">${d[i].AUTOR}</h5>
 			  </div>
 			  <div class="modal-body">
 			  <ul class="list-group">
-			  <li class="list-group-item list-group-item-primary d-flex justify-content-between">Titulo: <span class="badge bg-primary">${d[i].TITULO}</span></li>
-			  <li class="list-group-item list-group-item-secondary d-flex justify-content-between">Precio: <span class="badge bg-secondary">$ ${d[i].IMPORTE}</li>
-			  <li class="list-group-item list-group-item-success d-flex justify-content-between">Autor: <span class="badge bg-success">${d[i].AUTOR}</li>
-			  <li class="list-group-item list-group-item-danger d-flex justify-content-between">Editorial: <span class="badge bg-danger">${d[i].EDITORIAL}</li>
-			  <li class="list-group-item list-group-item-warning d-flex justify-content-between">ISBN: <span class="badge bg-warning">${d[i].ISBN}</li>
-			  <li class="list-group-item list-group-item-info d-flex justify-content-between">Medidas: <span class="badge bg-info">${d[i].MEDIDAS} cm</li>
+			  <li class="list-group-item list-group-item-light d-flex justify-content-between">Precio: <span class="badge bg-dark">$ ${d[i].IMPORTE}</li>
+			  <li class="list-group-item list-group-item-light d-flex justify-content-between">Editorial: <span class="badge bg-dark">${d[i].EDITORIAL}</li>
+			  <li class="list-group-item list-group-item-light d-flex justify-content-between">ISBN: <span class="badge bg-dark">${d[i].ISBN}</li>
 			  <li class="list-group-item list-group-item-light d-flex justify-content-between">Paginas: <span class="badge bg-dark"> ${d[i].PAG}</li>
-			  <li class="list-group-item list-group-item-dark d-flex justify-content-between">Codigo: <span class="badge bg-dark"> ${d[i].CODIGO}</li>
-			  <li class="list-group-item d-flex justify-content-between">Tematica: <span class="badge bg-primary"> ${d[i].TEMATICA}</span></li>
-			  <li class="list-group-item list-group-item-primary d-flex justify-content-between">Breve Reseña: <span class="badge bg-primary">${d[i].BREVE}</span></li>
+			  <li class="list-group-item list-group-item-light d-flex justify-content-between">Tematica: <span class="badge bg-dark"> ${d[i].TEMATICA}</span></li>
+			  <li class="list-group-item list-group-item-light d-flex justify-content-between">Breve Reseña: <span class="badge bg-dark">${d[i].BREVE}</span></li>
 			</ul>
 			  </div>
 			  <div class="modal-footer">
